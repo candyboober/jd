89d47e3ac0ce287b65682cbe7d45b8a1d308d934
candyboober <candyboobers@gmail.com>
2017-05-31 14:49:26 +0300

    add test for models

--- src/models/models_test.go
+++ src/models/models_test.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 )
 
+
 func TestVacancy(t *testing.T) {
 	// init params
 	db := DB{}
@@ -13,13 +14,30 @@ func TestVacancy(t *testing.T) {
 	// test create table
 	v.CreateTable(*db.Connect)
 	// test create obj
-	v = Vacancy{Title: "Some title", Body: "So long text body"}
+	data := map[string]string{"title": "Some title", "body": "So long text body"}
+	v = Vacancy{Title: data["title"], Body: data["body"]}
 	db.Connect.Create(&v)
+	if v.ID == 0 {
+		t.Error("ID of object is `0`")
+	}
 	// test update obj
-	db.Connect.Model(&v).Update("title", "Another title")
+	updatedField := "Another title"
+	db.Connect.Model(&v).Update("title", updatedField)
+	if v.Title != updatedField {
+		t.Error("Field wasn't updated")
+	}
 	// test get obj
 	id := strconv.FormatUint(uint64(v.ID), 16)
 	db.Connect.Where("id = ?", id).First(&v)
+	if v.ID == 0 {
+		t.Error("Object wasn't found")
+	}
 	// test delete obj
+	db.Connect.Delete(&v)
 
+	newV := Vacancy{}
+	db.Connect.Where("id = ?", id).First(&newV)
+	if newV.ID != 0 {
+		t.Error("Object was found, but has been deleted")
+	}
 }
